70d6ab235bf7c96230c60f2ef3addc1566e7323e
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2024-03-27 20:47:53 +1000

    meow

--- mellow/src/visual_scripting/mod.rs
+++ mellow/src/visual_scripting/mod.rs
@@ -32,17 +32,13 @@ pub struct ConditionalStatement {
 #[derive(Clone, Debug, Deserialize)]
 pub struct ConditionalStatementBlock {
 	pub items: Vec<Element>,
-	pub inputs: Vec<EventResponseInput>,
-	pub condition: Option<Condition>
+	pub inputs: Vec<StatementInput>,
+	pub condition: Option<StatementCondition>
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct Condition {
-	pub kind: ConditionKind
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub enum ConditionKind {
+#[serde(tag = "kind")]
+pub enum StatementCondition {
 	#[serde(rename = "generic.is")]
 	Is,
 	#[serde(rename = "generic.is_not")]
@@ -59,16 +55,16 @@ pub enum ConditionKind {
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(tag = "kind", content = "value", rename_all = "snake_case")]
-pub enum EventResponseInput {
+pub enum StatementInput {
 	Match(serde_json::Value),
 	Variable(String)
 }
 
-impl EventResponseInput {
-	fn resolve(&self, variables: &HashMap<String, serde_json::Value>) -> serde_json::Value {
+impl StatementInput {
+	fn resolve(&self, variables: &HashMap<String, serde_json::Value>) -> Option<serde_json::Value> {
 		match self {
-			EventResponseInput::Match(value) => value.clone(),
-			EventResponseInput::Variable(path) => {
+			StatementInput::Match(value) => Some(value.clone()),
+			StatementInput::Variable(path) => {
 				let mut value: Option<&serde_json::Value> = None;
 				for key in path.split("::") {
 					if let Some(val) = value {
@@ -78,13 +74,14 @@ impl EventResponseInput {
 					}
 				}
 	
-				value.unwrap().clone()
+				value.cloned()
 			}
 		}
 	}
 }
 
 pub struct ElementStream {
+	// would something else be better-suited for this?
 	iterator: Box<dyn Iterator<Item = Element> + Send>,
 	variables: HashMap<String, serde_json::Value>,
 	current_sub_stream: Option<Box<ElementStream>>,
@@ -141,9 +138,11 @@ impl ElementStream {
 	}
 
 	fn poll_sub_stream(&mut self, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Element>> {
+		// polls the sub stream for a potential element, otherwise, the current stream will continue iterating.
 		match self.current_sub_stream.as_mut().unwrap().get_next(cx) {
 			std::task::Poll::Ready(x) => match x {
 				Some(x) => return std::task::Poll::Ready(Some(x)),
+				// removes the sub stream, as it is now empty.
 				None => self.current_sub_stream = None
 			},
 			_ => ()
@@ -154,13 +153,14 @@ impl ElementStream {
 	fn poll_statement_stream(&mut self, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Element>> {
 		match self.current_statement_stream.as_mut().unwrap().get_next(cx) {
 			std::task::Poll::Ready(x) => match x {
-				Some(x) =>
-				self.current_sub_stream = Some(Box::new(ElementStream {
+				// creates a sub element stream iterating over the statement block's containing items.
+				Some(x) => self.current_sub_stream = Some(Box::new(ElementStream {
 					iterator: Box::new(x.items.into_iter()),
 					variables: self.variables.clone(),
 					current_sub_stream: None,
 					current_statement_stream: None
 				})),
+				// removes the statement stream, as it is now empty.
 				None => self.current_statement_stream = None
 			},
 			_ => ()
@@ -171,10 +171,8 @@ impl ElementStream {
 
 impl Stream for ElementStream {
 	type Item = Element;
-
 	fn poll_next(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
-		let stream = self.get_mut();
-		stream.get_next(cx)
+		self.get_mut().get_next(cx)
 	}
 
 	fn size_hint(&self) -> (usize, Option<usize>) {
@@ -191,18 +189,22 @@ impl StatementStream {
 	fn get_next(&mut self, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<ConditionalStatementBlock>> {
 		if let Some(block) = self.iterator.next() {
 			if let Some(condition) = &block.condition {
-				let variables = self.variables.clone();
-				let input_a = block.inputs.first().unwrap().resolve(&variables);
-				let input_b = block.inputs.get(1).unwrap().resolve(&variables);
-				if !match condition.kind {
-					ConditionKind::Is => input_a == input_b,
-					ConditionKind::IsNot => input_a != input_b,
-					ConditionKind::Contains => input_a.as_str().unwrap().contains(input_b.as_str().unwrap()),
-					ConditionKind::DoesNotContain => input_a.as_str().unwrap().contains(input_b.as_str().unwrap()),
-					ConditionKind::StartsWith => input_a.as_str().unwrap().starts_with(input_b.as_str().unwrap()),
-					ConditionKind::EndsWith => input_a.as_str().unwrap().ends_with(input_b.as_str().unwrap())
-				} {
-					return self.get_next(cx);
+				let variables = &self.variables;
+
+				// TODO: return an error if the inputs can't be resolved, said error should be logged to the server if possible.
+				if let Some(input_a) = block.inputs.first().and_then(|x| x.resolve(&variables)) {
+					if let Some(input_b) = block.inputs.get(1).and_then(|x| x.resolve(&variables))  {
+						if !match condition {
+							StatementCondition::Is => input_a == input_b,
+							StatementCondition::IsNot => input_a != input_b,
+							StatementCondition::Contains => input_a.as_str().unwrap().contains(input_b.as_str().unwrap()),
+							StatementCondition::DoesNotContain => input_a.as_str().unwrap().contains(input_b.as_str().unwrap()),
+							StatementCondition::StartsWith => input_a.as_str().unwrap().starts_with(input_b.as_str().unwrap()),
+							StatementCondition::EndsWith => input_a.as_str().unwrap().ends_with(input_b.as_str().unwrap())
+						} {
+							return self.get_next(cx);
+						}
+					}
 				}
 			}
 			std::task::Poll::Ready(Some(block))
@@ -213,8 +215,7 @@ impl StatementStream {
 impl Stream for StatementStream {
 	type Item = ConditionalStatementBlock;
 	fn poll_next(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
-		let stream = self.get_mut();
-		stream.get_next(cx)
+		self.get_mut().get_next(cx)
 	}
 
 	fn size_hint(&self) -> (usize, Option<usize>) {
